a9361923c83d55ee7f985666b3c6a124a7460527
piyush mehta <me@piyushmehta.com>
2020-04-03 18:10:50 +0530

    Cleanup
    Signed-off-by: piyush mehta <me@piyushmehta.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,5 @@
 import React, { useRef } from "react";
 import Helmet from "react-helmet";
-import L from "leaflet";
-import { Marker } from "react-leaflet";
-
-import { promiseToFlyTo, getCurrentLocation } from "lib/map";
-
 import Layout from "components/Layout";
 import Container from "components/Container";
 import Map from "components/Map";
@@ -17,65 +12,14 @@ const LOCATION = {
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
-const ZOOM = 10;
-
-const timeToZoom = 2000;
-const timeToOpenPopupAfterZoom = 4000;
-const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
-
-const popupContentHello = `<p>Hello 👋</p>`;
-const popupContentGatsby = `
-  <div class="popup-gatsby">
-    <div class="popup-gatsby-image">
-      <img class="gatsby-astronaut" src=${gatsby_astronaut} />
-    </div>
-    <div class="popup-gatsby-content">
-      <h1>Covid 19 World</h1>
-    </div>
-  </div>
-`;
 
 const IndexPage = () => {
-  const markerRef = useRef();
-
   /**
    * mapEffect
    * @description Fires a callback once the page renders
    * @example Here this is and example of being used to zoom in and set a popup on load
    */
-
-  async function mapEffect({ leafletElement } = {}) {
-    if (!leafletElement) return;
-
-    const popup = L.popup({
-      maxWidth: 800
-    });
-
-    const location = await getCurrentLocation().catch(() => LOCATION);
-
-    const { current = {} } = markerRef || {};
-    const { leafletElement: marker } = current;
-
-    marker.setLatLng(location);
-    popup.setLatLng(location);
-    popup.setContent(popupContentHello);
-
-    setTimeout(async () => {
-      await promiseToFlyTo(leafletElement, {
-        zoom: ZOOM,
-        center: location
-      });
-
-      marker.bindPopup(popup);
-
-      setTimeout(() => marker.openPopup(), timeToOpenPopupAfterZoom);
-      setTimeout(
-        () => marker.setPopupContent(popupContentGatsby),
-        timeToUpdatePopupAfterZoom
-      );
-    }, timeToZoom);
-  }
-
+  async function mapEffect({ leafletElement: map } = {}) {}
   const mapSettings = {
     center: CENTER,
     defaultBaseMap: "OpenStreetMap",
@@ -89,9 +33,7 @@ const IndexPage = () => {
         <title>Home Page</title>
       </Helmet>
 
-      <Map {...mapSettings}>
-        <Marker ref={markerRef} position={CENTER} />
-      </Map>
+      <Map {...mapSettings}></Map>
 
       <Container type="content" className="text-center home-start"></Container>
     </Layout>
