ce2537179bdbb2a874ac4d2d155f15a0489355f0
piyush mehta <me@piyushmehta.com>
2020-04-03 18:05:59 +0530

    INIT
    Signed-off-by: piyush mehta <me@piyushmehta.com>

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Container from "components/Container";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <p>Created with ❤ by Piyush97</p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Container from "components/Container";
+
+const Header = () => {
+  return (
+    <header>
+      <Container type="content">
+        <p>Covid 19 World</p>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
